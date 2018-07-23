9a690f1c22a6fbe8b303b9a710b5b3e38b7d012d
Anujin Munkhbat <anujin.munhbat@14four.com>
2018-07-23 09:34:19 -0700

    switched from bcrypt to bcryptjs

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,6 +1,6 @@
 const Connector = require('../database/connector')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 class UserController {
   constructor () {
